37659d08ca53b655dca71bf7db482b2a1585ce8d
Al Mamun <asifulmamun@gmail.com>
2018-11-21 17:39:13 +0600

    Initial commit

---
+++ feb_mms.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: FEBRUARY MEMBER MANAGEMENT SYSTEM
+Plugin URI: https://facebook.com/asifulmamun
+Description: This use for Mange Member
+Version: 1.0
+Author: Al Mamun - (asifulmamun)
+Author URI: https://facebook.com/asifulmamun
+*/
+
+ ?>
