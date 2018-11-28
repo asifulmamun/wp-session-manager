a255fd4f06cf84bce19c7a9a32b591dd54b54890
Al Mamun <asifulmamun@gmail.com>
2018-11-28 22:44:38 +0600

    ok

--- feb_mms.php
+++ feb_mms.php
@@ -8,7 +8,6 @@ Author: Al Mamun - (asifulmamun)
 Author URI: https://facebook.com/asifulmamun
 */
 
-
 function wpdocs_check_logged_in() {
     $current_user = wp_get_current_user();
     if ( 0 == $current_user->ID ) {
@@ -18,20 +17,21 @@ function wpdocs_check_logged_in() {
     } else {
         session_start(); // Start Session
 
-        /*
-			Here is variable with session declare and and value get from wp and passing data with session to feb-mms global plugin.
-        */
-        $_SESSION['feb_wp_get_current_user'] = $current_user; // user Id
-        $_SESSION['feb_home'] =  esc_url( home_url( '/' ) ); // Home URL
-        $_SESSION['power'] =$user_meta->roles; // User Role
-		
-
-
-
+        /**
+		*	@Start Here Variable with Session
+		*
+		*	Here is variable with session declare and and value get from wp and passing data with session to feb-mms global plugin.
+		*
+        **/
+        $_SESSION['feb_wp_get_current_user_id'] = $current_user; // user Id
+        $_SESSION['feb_main_web_home'] =  esc_url( home_url( '/' ) ); // Home URL Main Web Site
+        // User Roles
+	    $user_roles = $current_user->roles;
+	    $user_role = array_shift($user_roles);
+		$_SESSION['power'] = $user_role; // set user role in session
 
     }
 }
 add_action( 'init', 'wpdocs_check_logged_in' );
-
 
  ?>
